fb7b5d693c449ae7f95c2c4195767f5ab130726d
Adish K T <adishadi01@gmail.com>
2026-08-10 14:26:46 +0000

    Handle Hasura Action input

--- functions/triggerWorkflowRun.js
+++ functions/triggerWorkflowRun.js
@@ -1,7 +1,11 @@
-export default (req, res) => {
+import type { Request, Response } from "express";
+
+export default (req: Request, res: Response) => {
+  const workflowId = req.body?.input?.workflow_id ?? null;
+
   res.status(200).json({
     success: true,
     message: "triggerWorkflowRun is working",
-    workflow_id: req.body?.workflow_id ?? null,
+    workflow_id: workflowId,
   });
 };
